b43e6e31b6e74598485bd4929962be93aae86865
Butchy1596 <dterra0726@gmail.com>
2018-03-14 23:52:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay (musicians, instruments){
   var musicians =[];
-  for (var i = 0, i < 5, i++){
+  for (var i = 0; i < 5; i++){
   theBeatlesPlay.push("John Lennon plays guitar")
   }
 }
