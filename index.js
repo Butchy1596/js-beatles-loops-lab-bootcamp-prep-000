3a7a02fd314339def5b11eea2f437b16ff6ccbea
Butchy1596 <dterra0726@gmail.com>
2018-03-15 00:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ function theBeatlesPlay (musicians, instruments){
 
 
 function johnLennonFacts (){
-  var facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
-
-  ];
+  var facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
   var newFacts = [];
   var i = 0;
   while (i < facts.length){
@@ -25,3 +19,5 @@ function johnLennonFacts (){
   }
   return newFacts;
 }
+
+
