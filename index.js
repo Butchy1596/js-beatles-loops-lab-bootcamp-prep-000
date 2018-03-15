b1a6ca956fdfdedb7eaf74a2a1d75ad069c6dd65
Butchy1596 <dterra0726@gmail.com>
2018-03-15 00:47:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function johnLennonFacts (){
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
-    "He hated the sound of his own voice",
-    ""
+    "He hated the sound of his own voice"
+    
   ];
   var newFacts = [];
   var i = 0;
   while (i < facts.length){
-    newFacts.push(facts[i] + "!!!");
+    newFacts.push(facts[i] + " !!!");
     i++
   }
   return newFacts;
